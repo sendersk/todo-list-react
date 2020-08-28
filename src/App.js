3ae5f0369a38e7e219f6d422583aeb726d7d2c62
Przemyslaw Senderski <psenderski94@gmail.com>
2020-08-28 18:10:37 +0200

    New content added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const tasks = [
+  {id: 1, content: "Przejść na Reacta", done: false},
+  {id: 2, content: "Obejrzeć serial", done: true},
+];
+
+const hideDoneTasks = false;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="container">
+      <header>
+        <h1>Lista zadań</h1>
       </header>
-    </div>
+
+      <section className="section">
+        <h2 className="section__header">Dodaj nowe zadanie</h2>
+        <div className="section__body">
+          <form className="form">
+            <input
+              className="form__input"
+              placeholder="Co jest do zrobienia?"
+            />
+            <button className="form__button">Dodaj zadanie</button>
+          </form>
+        </div>
+      </section>
+      <section className="section">
+        <header className="section__header">
+          <h2 className="section__title">Lista zadań</h2>
+          <div className="buttons"></div>
+        </header>
+        <div className="section__body">
+          <ul className="tasks"></ul>
+        </div>
+      </section>
+    </main>
   );
 }
 
