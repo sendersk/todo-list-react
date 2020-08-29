bbbe4e98cca26c1d9e31bba2d63f13b68f6858f7
Przemyslaw Senderski <psenderski94@gmail.com>
2020-08-29 09:58:35 +0200

    Updates

---
+++ precache-manifest.c16823d0dc42c1bec0d3cae5b8f27e10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a7ae834e974f27b62cc4c795e0aed9b",
+    "url": "/todos-list-react/index.html"
+  },
+  {
+    "revision": "87dc2c03f451531e8ff2",
+    "url": "/todos-list-react/static/css/main.0ad9726b.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/todos-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87dc2c03f451531e8ff2",
+    "url": "/todos-list-react/static/js/main.9e84592e.chunk.js"
+  },
+  {
+    "revision": "97d8fa25e5bde6a122ac",
+    "url": "/todos-list-react/static/js/runtime-main.bb6efc59.js"
+  }
+]);
