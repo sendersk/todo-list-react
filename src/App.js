8d87f3214d08284c6d396a99486928c06af3b497
Przemyslaw Senderski <psenderski94@gmail.com>
2020-08-30 13:40:54 +0200

    add new task interaction added

--- src/App.js
+++ src/App.js
@@ -36,13 +36,24 @@ function App() {
       ...task, done: true
     })));
   };
+
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks, 
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ]);
+  };
   
   return (
     <Container>
       <Header title="Lista zadań" />
       <Section 
         title="Dodaj nowe zadanie" 
-        body={ <Form />}
+        body={ <Form addNewTask={addNewTask} />}
       />
       <Section
         title="Lista zadań"
