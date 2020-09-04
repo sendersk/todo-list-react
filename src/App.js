5cd2a19c6fda13eb419f1509fb0c207ff81a41f6
Przemyslaw Senderski <psenderski94@gmail.com>
2020-09-04 19:25:29 +0200

    Global styles added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
+import { GlobalStyle } from "./GlobalStyle";
 
 const getInitialTasks = () => {
   const tasksFromLocalStorage = localStorage.getItem("tasks");
@@ -62,6 +63,7 @@ function App() {
   
   return (
     <Container>
+      <GlobalStyle />
       <Header title="Lista zadań" />
       <Section 
         title="Dodaj nowe zadanie" 
