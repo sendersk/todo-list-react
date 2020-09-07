065e0fa16b5b28ede591e0323b7e2f11175faff1
Przemyslaw Senderski <psenderski94@gmail.com>
2020-09-07 20:18:21 +0200

    application split into features

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
-import App from './App';
+import App from "./App";
 import * as serviceWorker from './serviceWorker';
 import { theme } from "./theme";
 
