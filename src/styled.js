984da995b18063f7992922af6036dca296935321
Przemyslaw Senderski <psenderski94@gmail.com>
2020-09-19 19:21:12 +0200

    Navigation styles added

---
+++ src/styled.js
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+const activeClassName = "active";
+
+export const StyledNavLink = styled(NavLink).attrs(() => ({
+    activeClassName,
+}))`
+    font-size: 20px;
+    text-decoration: none;
+    background-color: ${({theme}) => theme.color.teal};
+    color: ${({theme}) => theme.color.white};
+    padding: 10px;
+
+    &.${activeClassName} {
+        filter: brightness(85%);
+        font-weight: bold;
+    }
+`;
+
+export const Navigation = styled.ul`
+    background-color: ${({theme}) => theme.color.teal};
+    display: flex;
+    justify-content: center;
+    margin: 0;
+    padding: 0;
+`;
+
+export const Item = styled.li`
+    list-style: none;
+    padding: 20px;
+`;
