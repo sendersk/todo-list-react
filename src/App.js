536163e2ebf2fd7d6e0e2cbdd8df354fcf6903ef
Przemyslaw Senderski <psenderski94@gmail.com>
2020-09-21 20:34:41 +0200

    Navigation section modified

--- src/App.js
+++ src/App.js
@@ -3,33 +3,26 @@ import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import TasksPage from "./features/tasks/TasksPage";
 import TaskPage from "./features/tasks/TaskPage";
 import AboutPage from "./features/about/AboutPage";
-import { StyledNavLink, Navigation, Item } from "./styled";
+import { toAbout, toTask, toTasks } from "./routes";
+import Navigation from "./Navigation";
 
 export default () => (
   <HashRouter>
-    <nav>
-      <Navigation>
-          <Item>
-            <StyledNavLink to="/tasks">Tasks list</StyledNavLink>
-          </Item>
-          <Item>
-            <StyledNavLink to="/about">About</StyledNavLink>
-          </Item>
-      </Navigation>
+      <Navigation />
+      
       <Switch>
-        <Route path="/tasks/:id">
+        <Route path={toTask()}>
           <TaskPage />
         </Route>
-        <Route path="/tasks">
+        <Route path={toTasks()}>
           <TasksPage />
         </Route>
-        <Route path="/about">
+        <Route path={toAbout()}>
           <AboutPage />
         </Route>
-        <Route path="/">
-          <Redirect to="/tasks" />
+        <Route>
+          <Redirect to={toTasks()} />
         </Route>
       </Switch>
-    </nav>
   </HashRouter>
 );
