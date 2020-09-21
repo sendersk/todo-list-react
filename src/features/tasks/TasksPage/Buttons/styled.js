072cc34eaa37f5dab367ac25ed9f225985b05f22
Przemyslaw Senderski <psenderski94@gmail.com>
2020-09-21 20:35:07 +0200

    Button removed

--- src/features/tasks/TasksPage/Buttons/styled.js
+++ src/features/tasks/TasksPage/Buttons/styled.js
@@ -4,28 +4,3 @@ export const Wrapper = styled.div`
     display: flex;
     flex-wrap: wrap;
 `;
-
-export const Button = styled.button`
-    background: transparent;
-    color: ${({ theme }) => theme.color.teal};
-    border: none;
-    margin: 0 0 0 20px;
-    transition: color 0.3s;
-
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        flex-basis: 100%;
-        margin: 10px;
-    }
-
-    &:hover {
-        filter: brightness(110%);
-    }
-
-    &:active {
-        filter: brightness(120%);
-    }
-
-    &:disabled {
-        color: ${({ theme }) => theme.color.silver};
-    }
-`;
